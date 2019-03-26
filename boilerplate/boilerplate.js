c3d43f885f7cfc5d24f83162a67b1ee2740f8b0a
Michael Neth <michael@michaelneth.com>
2019-03-26 14:27:28 -0500

    Init eslint and resolve fixes

--- boilerplate/boilerplate.js
+++ boilerplate/boilerplate.js
@@ -1,24 +1,27 @@
 const boilerplate = {
-    hello () {
-        return "Hello world";
-    },
-    obj () {
-        return {
-            hello: this.hello()
-        }
-    },
-    arr () {
-        return [1,2,3,4,5,6];
-    },
-    err () {
-        throw new Error("Expected test error");
-    },
-    asyncPromise () {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => resolve("Times up"), 500);
-            // setTimeout(() => reject("Error"), 500);
-        });
-    },
+  hello() {
+    return 'Hello world';
+  },
+  obj() {
+    return {
+      hello: this.hello(),
+    };
+  },
+  arr() {
+    return [1, 2, 3, 4, 5, 6];
+  },
+  err() {
+    throw new Error('Expected test error');
+  },
+  asyncPromise() {
+    return new Promise((resolve, reject) => {
+      try {
+        setTimeout(() => resolve('Times up'), 500);
+      } catch (e) {
+        reject(new Error('Error'));
+      }
+    });
+  },
 };
 
 module.exports = boilerplate;
