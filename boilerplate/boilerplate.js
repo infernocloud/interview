a21eb58e889b490c0c65588b84b64eae031593cd
Michael Neth <michael@michaelneth.com>
2019-03-28 23:30:12 -0500

    Prettier formatting

--- boilerplate/boilerplate.js
+++ boilerplate/boilerplate.js
@@ -1,27 +1,27 @@
 const boilerplate = {
   hello() {
-    return 'Hello world';
+    return "Hello world";
   },
   obj() {
     return {
-      hello: this.hello(),
+      hello: this.hello()
     };
   },
   arr() {
     return [1, 2, 3, 4, 5, 6];
   },
   err() {
-    throw new Error('Expected test error');
+    throw new Error("Expected test error");
   },
   asyncPromise() {
     return new Promise((resolve, reject) => {
       try {
-        setTimeout(() => resolve('Times up'), 500);
+        setTimeout(() => resolve("Times up"), 500);
       } catch (e) {
-        reject(new Error('Error'));
+        reject(new Error("Error"));
       }
     });
-  },
+  }
 };
 
 module.exports = boilerplate;
